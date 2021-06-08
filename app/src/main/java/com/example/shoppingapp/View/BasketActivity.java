296b874a3bc602f500c018fe2adff9e83315aa37
kasasSulaiman <s195462@student.dtu.dk>
2021-06-08 14:15:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shoppingapp/View/BasketActivity.java
@@ -0,0 +1,29 @@
+package com.example.shoppingapp.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.shoppingapp.R;
+
+public class BasketActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basket);
+
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/shoppingapp/View/HomeActivity.java
@@ -0,0 +1,58 @@
+package com.example.shoppingapp.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.shoppingapp.R;
+import com.example.shoppingapp.controller.RecyclerAdapter;
+import com.example.shoppingapp.model.ProductCollection;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private RecyclerView  productRecyclerView;
+    private ImageView basket;
+
+    private RecyclerAdapter adapter;
+
+
+  /*  private ProductListAdapter productListAdapter;
+    private List<Product> productList;
+
+   */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        productRecyclerView = findViewById(R.id.productRecyclerViewId);
+        basket = findViewById(R.id.imageView2);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
+        productRecyclerView.setLayoutManager(layoutManager);
+        //adapter = new RecyclerAdapter(products, this);
+        productRecyclerView.setAdapter(new RecyclerAdapter(ProductCollection.getSpaceships(),this));
+
+
+
+        basket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, BasketActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+
+
+
+
+}
